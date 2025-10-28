c7bb4df56b845e4bb989e857ec6aefb3714eb6f6
Andrew Segerlind Home Computer 'LINAPC <andrew.segerlind@gmail.com>
2025-10-27 21:20:42 -0700

    Setting up venv

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # red-vid-repost
 repost everything. Maybe add captions
+
+TODO
+# Segment/clip/partition the video
+
+
+flow:
+
+find reddit post
+
+if reddit post not in previously used (TOML FILE):
+    Download the video
+    optional (add captions)
+    post the video 
---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Starting the application..."
+
+log_file=logs/$(date '+%Y-%m-%d')_app.log
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $log_file
+}
+
+
+log "Activating virtual environment..."
+python3 -m venv myenv
+
+source myenv/bin/activate
+
+log "Running the Python application..."
+python3 main.py
+
+deactivate
+log "Deactivating virtual environment..."
